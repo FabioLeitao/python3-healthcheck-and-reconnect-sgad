dd902542273e46949432831469bd270ab24de037
Fabio Leitao <fabio.tleitao@gmail.com>
2024-01-13 04:09:19 -0300

    Melhoria no teste condicional por lista ao inves de dois testes e inclusao de compactados no gitignore

--- healthcheck_sgad.py
+++ healthcheck_sgad.py
@@ -16,9 +16,7 @@ if connection.is_healthy():
         with connection.cursor() as cursor:
             for ID, KEY, VALUE, UPDATED in cursor.execute(select_sql):
                     print(ID, KEY, VALUE, UPDATED)
-                    if VALUE == "false":
-                        Corrige_SGAD()
-                    if VALUE == "falso":
+                    if VALUE in [ "false", "falso"]:
                         Corrige_SGAD()
         connection.close()
     pool.close()
