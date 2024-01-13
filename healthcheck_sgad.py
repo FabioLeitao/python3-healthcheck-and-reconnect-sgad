f0a8f6b59daf0e63fc0b097b6782f77f409c3a8d
Fabio Leitao <fabio.tleitao@gmail.com>
2024-01-13 02:39:20 -0300

    Limpeza do codigo, inclusao de comentários e melhorias de texto ecoado em caso de erro de acesso ao banco

--- healthcheck_sgad.py
+++ healthcheck_sgad.py
@@ -1,11 +1,12 @@
 #!/usr/local/bin/python3.12
 import oracledb
 
-#conn = oracledb.connect(user="tosp", password="Athenas2018", dsn="10.129.48.68/tosprd")
+# Criando pool de conexões caso seja necessario algum paralelismo no futuro
 pool = oracledb.create_pool(user="tosp", password="Athenas2018", dsn="10.129.48.68/tosprd", min=1, max=5, increment=1)
 connection = pool.acquire()
 pool.wait_timeout = 1000
 if connection.is_healthy():
+    # Linhas para possiveis coletas de informacoes do banco de dados, util em caso de debug
     #print("Healthy connection!")
     #print(oracledb.__version__)
     #print("Database version:", connection.version)
@@ -22,7 +23,7 @@ if connection.is_healthy():
         connection.close()
     pool.close()
 else:
-    print("Unusable connection. Please check the database and network settings.")
+    print("Error - Unusable connection. Please check the database and network settings.")
 
 def Corrige_SGAD():
     update_sql = "UPDATE CORE.cosetting SET VALUE = 'TRUE' WHERE id = 681"
