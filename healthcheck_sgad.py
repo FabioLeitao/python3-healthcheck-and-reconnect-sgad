9c75f8d1360e2e5953eefc81e6e0c28241663098
Fabio Leitao <fabio.tleitao@gmail.com>
2024-01-20 09:08:04 -0300

    Corrigida logica em caso de false para acertar o SGAD sem erro de commit ou connection

--- healthcheck_sgad.py
+++ healthcheck_sgad.py
@@ -5,6 +5,16 @@ import os
 # Lendo senha de variavel do ambiente
 userpwd = os.environ.get("PYTHON_DB_PASSWORD")
 
+def Corrige_SGAD(): 
+    connection = pool.acquire() 
+    pool.wait_timeout = 1000
+    if connection.is_healthy():
+        update_sql = "UPDATE CORE.cosetting SET VALUE = 'TRUE' WHERE id = 681" 
+        with pool.acquire() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(update_sql) 
+                connection.commit ()
+
 # Criando pool de conexões caso seja necessario algum paralelismo no futuro
 pool = oracledb.create_pool(user="tosp", password=userpwd, dsn="10.129.48.68/tosprd", min=1, max=5, increment=1)
 connection = pool.acquire()
@@ -19,14 +29,15 @@ if connection.is_healthy():
     with pool.acquire() as connection:
         with connection.cursor() as cursor:
             for ID, KEY, VALUE, UPDATED in cursor.execute(select_sql):
+                if VALUE == "TRUE":
                     print(ID, KEY, VALUE, UPDATED)
-                    if VALUE in [ "false", "falso" ]:
-                        Corrige_SGAD()
+                    #print("Correto")
+                else:
+                    print(ID, KEY, VALUE, UPDATED)
+                    #print("Corrigindo SGAD")
+                    Corrige_SGAD()
         connection.close()
     pool.close()
 else:
     print("Error - Unusable connection. Please check the database and network settings.")
 
-def Corrige_SGAD():
-    update_sql = "UPDATE CORE.cosetting SET VALUE = 'TRUE' WHERE id = 681"
-    cursos.execute(update_sql);
